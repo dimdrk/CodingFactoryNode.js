ba1be84bc4792e7edd3b9fa35a4f3d96c9703ebc
DimDrk <dimdrk@gmail.com>
2024-09-30 22:28:54 +0300

    added 'create'

--- products/index.js
+++ products/index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 mongoose.connect(process.env.MONGODB_URI)
     .then(
         () => { console.log('Connection to MongoDB established.')},
