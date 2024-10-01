7a3d2f9979e8c06a60a2c5bd737d30ec9d1b0bf9
DimDrk <dimdrk@gmail.com>
2024-10-01 19:46:22 +0300

    added delete controller

--- products/routes/user.routes.js
+++ products/routes/user.routes.js
@@ -6,6 +6,7 @@ const userController = require('../controllers/user.controller');
 router.get('/', userController.findAll);
 router.get('/:username', userController.findOne);
 router.post('/', userController.create);
-router.patch('/:username', userController.update)
+router.patch('/:username', userController.update);
+router.delete('/:username', userController.delete);
 
 module.exports = router;
