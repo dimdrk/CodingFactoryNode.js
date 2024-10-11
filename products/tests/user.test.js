c687db9bc53afd590daa29aab00a0568eb128b15
DimDrk <dimdrk@gmail.com>
2024-10-11 19:20:27 +0300

    updates

--- products/tests/user.test.js
+++ products/tests/user.test.js
@@ -22,9 +22,9 @@ describe("Tests for /api/users requests", () => {
         expect(result.statusCode).toBe(200);
         expect(result.body.status).toBeTruthy();
         expect(result.body.data.length).toBeGreaterThan(0);
-    }, 10000);
+    });
 
-    it("POST /api/users", async() => {
+    it("POST /api/users request", async() => {
         const result = await request(app).post("/api/users")
             .send({
                 username: "test4",
@@ -41,20 +41,58 @@ describe("Tests for /api/users requests", () => {
         expect(result.statusCode).toBe(200);
         expect(result.body.status).toBeTruthy();
         expect(result.body.data).toBeTruthy();
-    }, 10000);
+    });
+
+    it("POST /api/users request check for existing user", async() => {
+        const result = await request(app).post("/api/users")
+            .send({
+                username: "test4",
+                password: "12345",
+                name: "test4name",
+                surname: "test4surname",
+                email: "test4@aueb.gr",
+                address: {
+                    area: "area44",
+                    road: "road44"
+                }
+            });
+
+        expect(result.statusCode).toBe(200);
+        expect(result.body.status).toBeFalsy();
+    });
 });
 
 describe("For /api/users/{username} requests", () => {
     it("GET /api/users/{username}", async() => {
         const res = await helpers.findLastInsertedUser();
-        console.log(res.username);
+        console.log(res);
         const result = await request(app).get('/api/users/' + res.username);
 
         expect(result.statusCode).toBe(200);
         expect(result.body.status).toBeTruthy();
         expect(result.body.data.username).toBe(res.username);
         expect(result.body.data.email).toBe(res.email);
-    }, 10000);
+    });
+
+    it("PATCH for api/users/{username}", async () => {
+        const result = await helpers.findLastInsertedUser();
+        const res = await request(app)
+            .patch('/api/users/' + result.username)
+            .send({
+                name: "new test4",
+                surname: "new test4",
+                email: "xxx@aueb.gr",
+                address: {
+                    area: "new area",
+                    road: "new road"
+                }
+            });
+        console.log(res.body.data);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.status).toBeTruthy();
+        expect(res.body.data.name).toBe("new test4");
+        expect(res.body.data.surname).toBe("new test4");
+    })
 
     it("DELETE /api/users/{username}", async() => {
         const res = await helpers.findLastInsertedUser();
@@ -63,5 +101,5 @@ describe("For /api/users/{username} requests", () => {
 
         expect(result.statusCode).toBe(200);
         expect(result.body.status).toBeTruthy();
-    }, 10000);
+    });
 });
