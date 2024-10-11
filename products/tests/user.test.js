5e926048137af4ede86da9fa022ef5512bbf7e55
DimDrk <dimdrk@gmail.com>
2024-10-11 14:14:38 +0300

    test updates

--- products/tests/user.test.js
+++ products/tests/user.test.js
@@ -3,7 +3,7 @@ const request = require("supertest");
 const app = require('../index');
 
 beforeEach(async () => {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGODB_URI)
         .then(
             () => { console.log("Connection to Mongo from Jest established.") },
             err => { console.log("Failed to connect to Mongo from Jest.") },
@@ -14,10 +14,49 @@ afterEach(async () => {
     await mongoose.connection.close();
 })
 
-describe("For /api/users requests", () => {
+describe("Tests for /api/users requests", () => {
+    it("GET /api/users", async() => {
+        const result = await request(app).get("/api/users");
 
+        expect(result.statusCode).toBe(200);
+        expect(result.body.status).toBeTruthy();
+        expect(result.body.data.length).toBeGreaterThan(0);
+    }, 10000);
+
+    it("POST /api/users", async() => {
+        const result = await request(app).post("/api/users")
+            .send({
+                username: "test4",
+                password: "12345",
+                name: "test4name",
+                surname: "test4surname",
+                email: "test4@aueb.gr",
+                address: {
+                    area: "area44",
+                    road: "road44"
+                }
+            });
+
+        expect(result.statusCode).toBe(200);
+        expect(result.body.status).toBeTruthy();
+        expect(result.body.data).toBeTruthy();
+    }, 10000);
 });
 
 describe("For /api/users/{username} requests", () => {
+    it("GET /api/users/{username}", async() => {
+        const result = await request(app).get("/api/users/test4");
 
+        expect(result.statusCode).toBe(200);
+        expect(result.body.status).toBeTruthy();
+        expect(result.body.data.username).toBe('test4');
+        expect(result.body.data.email).toBe('test4@aueb.gr');
+    }, 10000);
+
+    it("DELETE /api/users/{username}", async() => {
+        const result = await request(app).delete("/api/users/test4");
+
+        expect(result.statusCode).toBe(200);
+        expect(result.body.status).toBeTruthy();
+    }, 10000);
 });
